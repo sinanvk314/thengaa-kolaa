1cc26830f50202a512c7be96d519ba7f4a2a75ae
sinan <sinanvk314@gmail.com>
2026-05-25 21:35:43 +0530

    added copper

---
+++ animated_sprite_2d.gd
@@ -0,0 +1,66 @@
+extends CharacterBody2D
+
+
+const SPEED = 100.0
+const JUMP_VELOCITY = -250.0
+const MAX_FALL_SPEED = 400
+
+var direction = 1
+var jump_count = 0
+var gravity_strength = 980
+
+
+
+func _physics_process(delta: float) -> void:
+	
+	handle_jump()
+	handle_movement()
+	move_and_slide()
+	apply_gravity(delta)
+	update_animation()
+
+
+func handle_jump() -> void:
+	
+	if is_on_floor():
+		jump_count=0
+	if jump_count == 0 and Input.is_action_just_pressed("jump"):
+		velocity.y = JUMP_VELOCITY
+		jump_count += 1
+	elif jump_count == 1 and Input.is_action_just_pressed("jump"):
+		velocity.y = JUMP_VELOCITY
+		jump_count += 1
+	if Input.is_action_just_released("jump") and velocity.y < 0:
+		velocity.y *= 0.3
+
+
+func apply_gravity(delta: float) -> void:
+	if not is_on_floor() and velocity.y < MAX_FALL_SPEED:
+		velocity.y += gravity_strength * delta
+
+
+func handle_movement() -> void:
+	direction = Input.get_axis("ui_left", "ui_right") 
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+
+func update_animation() -> void:
+	if direction==-1:
+	
+		$visuals.scale.x = -1
+		
+	elif direction == 1:
+		$visuals.scale.x = 1
+		
+	
+	if not is_on_floor():
+		$AnimationPlayer.play("jump")
+	
+	elif abs(velocity.x) > 5:
+		$AnimationPlayer.play("run")
+	
+	else:
+		$AnimationPlayer.play("idle")
